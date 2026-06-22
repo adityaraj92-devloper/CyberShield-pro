e445ca79afc6b8c05a4ea9d629b46883aba7051a
adityaraj92-devloper <mishraaditya2155@gmail.com>
2026-06-23 03:04:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,6 +107,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 # User Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -469,8 +472,4 @@ def logout():
 
 # Run
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()
-
     app.run(debug=True)
